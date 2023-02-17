c1a117e2365a7506d7dc159ed6dd6d5bba257524
Viktor Seniukov <spinnekopser@gmail.com>
2023-02-17 16:19:39 +0600

    Function isPalindrome

--- js/functions.js
+++ js/functions.js
@@ -1,13 +1,31 @@
 // First function
 
-const userString = 'abs';
-const validLength = 5;
+// const userString = 'abs';
+// const validLength = 5;
 
-function getValid(string, length) {
-  if (string.length >= length) {
+// function isValid(string, length) {
+//   if (string.length >= length) {
+//     return true;
+//   }
+//   return false;
+// }
+
+// isValid(userString, validLength);
+
+// Second function
+
+const userString = 'Лёша на полке клопа нашёл ';
+
+function isPalindrome (string) {
+  let newString = '';
+  for (let i = string.length - 1; i > -1; i--) {
+    newString += string.at(i);
+  }
+  console.log(newString);
+  if (newString.toLowerCase().replaceAll(' ', '') === string.toLowerCase().replaceAll(' ', '')) {
     return true;
   }
   return false;
 }
 
-getValid(userString, validLength);
+isPalindrome(userString);
