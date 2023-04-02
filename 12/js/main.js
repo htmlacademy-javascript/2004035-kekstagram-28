acbcc493b51d9807ffc6b09395213685b6fbf7c9
Keks <keks@htmlacademy.ru>
2023-04-02 20:57:48 +0300

    :heavy_check_mark: Сборка #12

---
+++ 12/js/main.js
@@ -0,0 +1,30 @@
+import { createThumbnails } from './thumbnails.js';
+import { addPicturesClickListener } from './open-big-picture.js';
+import { setOpenFormListener } from './open-close-upload-form.js';
+import { getData } from './api.js';
+import { initSuccesseAlert, initFailAlert } from './open-close-form-alert.js';
+import { showFiltersBlock, setClickFilter, onFilterClick } from './filters.js';
+import { debounce } from './util.js';
+
+const TIMEOUT_DELAY = 500;
+
+getData()
+  .then((posts) => {
+    createThumbnails(posts);
+    return posts;
+  })
+  .then((data) => {
+    showFiltersBlock();
+    setClickFilter(
+      debounce(
+        (evt) => onFilterClick(data, evt),
+        TIMEOUT_DELAY
+      )
+    );
+    return data;
+  })
+  .then((data) => addPicturesClickListener(data));
+
+initSuccesseAlert();
+initFailAlert();
+setOpenFormListener();
